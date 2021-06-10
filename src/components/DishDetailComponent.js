6b4d1a6b81ebb428a5a7390544ae9f3be9457be3
MadushikaAmarasekara <madushirenes@gmail.com>
2021-06-10 07:49:45 +0530

    React Animation Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -12,37 +13,57 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 function RenderDish({ dish }){
     return(
         <div className="col-12 col-md-5 m-1">
-            <Card>
-                <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
+                <Card>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         </div>
     );     
 }
 
 function RenderComments({ comments, postComment, dishId }){
     if(comments!=null){
-        // let dt = { year: "numeric", month: "short", day: "numeric" };
-
-        const coms = comments.map((comment) => { 
-            return(
-                <ul className="list-unstyled">
-                    <li key={comment.id}>{comment.comment}</li>
-                    <li className="mt-2">-- {comment.author} , {new Intl.DateTimeFormat('en-US', {year: "numeric", month: "short", day: "2-digit"}).format(new Date(Date.parse(comment.date)))}</li> {/*{new Date(comment.date).toLocaleDateString("en-US", dt)} */}
-                </ul>
-            );
-        });
-
         return(
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
-                {coms}
+                <ul className="list-unstyled">
+                    <Stagger in>
+                        {comments.map((comment) => {
+                            return(
+                                <Fade in>
+                                    <li key={comment.id}>
+                                        <p>{comment.comment}</p>
+                                        <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                    </li>
+                                </Fade>
+                            );
+                        })}
+                    </Stagger>
+                </ul>
+                {/* {coms} */}
                 <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
+
+        // let dt = { year: "numeric", month: "short", day: "numeric" };
+
+        // const coms = comments.map((comment) => { 
+        //     return(
+        //         <ul className="list-unstyled">
+        //             <Stagger in></Stagger>
+        //             <li key={comment.id}>{comment.comment}</li>
+        //             <li className="mt-2">-- {comment.author} , {new Intl.DateTimeFormat('en-US', {year: "numeric", month: "short", day: "2-digit"}).format(new Date(Date.parse(comment.date)))}</li> {/*{new Date(comment.date).toLocaleDateString("en-US", dt)} */}
+        //         </ul>
+        //     );
+        // });
     }
     else{
         return(
